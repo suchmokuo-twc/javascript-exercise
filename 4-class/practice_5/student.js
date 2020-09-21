c8ca8166b06adc050d188d1e38d03bad22c39773
SUCHMOKUO <suchmokuo@gmail.com>
2020-09-21 17:13:09 +0800

    [liuxing] implement class_practice_5

--- 4-class/practice_5/student.js
+++ 4-class/practice_5/student.js
@@ -1,1 +1,16 @@
-// TODO 19: 在这里写实现代码
+import Person from './person';
+
+export default class Student extends Person {
+  constructor(name, age, klass) {
+    super(name, age);
+
+    this.klass = klass;
+  }
+
+  introduce() {
+    const { klass } = this;
+    const personIntroduce = super.introduce();
+
+    return `${personIntroduce} I am a Student. I am at Class ${klass}.`;
+  }
+}
--- 4-class/practice_5/teacher.js
+++ 4-class/practice_5/teacher.js
@@ -1,1 +1,17 @@
-// TODO 20: 在这里写实现代码
+import Person from './person';
+
+export default class Teacher extends Person {
+  constructor(name, age, klass) {
+    super(name, age);
+
+    this.klass = klass;
+  }
+
+  introduce() {
+    const { klass } = this;
+    const personIntroduce = super.introduce();
+    const classIntroduce = typeof klass === 'undefined' ? 'No Class' : `Class ${klass}`;
+
+    return `${personIntroduce} I am a Teacher. I teach ${classIntroduce}.`;
+  }
+}
