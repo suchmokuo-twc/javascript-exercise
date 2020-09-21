1aabc67c706a35bd178b9033e3c88322d017f97f
SUCHMOKUO <suchmokuo@gmail.com>
2020-09-21 18:02:55 +0800

    [liuxing] implement async

--- 5-async/4-async/index.js
+++ 5-async/4-async/index.js
@@ -1,8 +1,20 @@
 async function fetchData(url) {
   // <-- start
   // TODO 24: 通过await/async实现异步请求
+
+  const response = await fetch(url);
+  const json = await response.json();
+
+  return json;
+
   // end -->
 }
 
 const URL = 'http://localhost:3000/api';
-fetchData(URL);
+fetchData(URL)
+  .then(result => {
+    document.writeln(result.name);
+  })
+  .catch(error => {
+    console.error(error);
+  });
