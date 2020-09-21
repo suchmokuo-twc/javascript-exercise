fdee35da899178ba9bf61f4a891981b922df5161
SUCHMOKUO <suchmokuo@gmail.com>
2020-09-21 17:07:13 +0800

    [liuxing] implement class_practice_4

--- 4-class/practice_4/worker.js
+++ 4-class/practice_4/worker.js
@@ -1,1 +1,9 @@
-// TODO 17: 在这里写实现代码
+import Person from './person';
+
+export default class Worker extends Person {
+  introduce() {
+    const personIntroduce = super.introduce();
+
+    return `${personIntroduce} I am a Worker. I have a job.`;
+  }
+}
