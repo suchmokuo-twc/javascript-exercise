31c2fe3c9bc822b9b1eb92a72b5e458743856ea4
SUCHMOKUO <suchmokuo@gmail.com>
2020-09-21 16:32:18 +0800

    [liuxing] implement find

--- 2-array/find/index.js
+++ 2-array/find/index.js
@@ -1,3 +1,9 @@
 export default function find00OldPerson(collection) {
-  // TODO 4: 在这里写实现代码
+  const person = collection.find(p => p.age <= 20);
+
+  if (!person) {
+    throw new Error('person not found');
+  }
+
+  return person.name;
 }
