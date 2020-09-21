17dc971c39a57922b701eb240c737194ef57fe08
SUCHMOKUO <suchmokuo@gmail.com>
2020-09-21 16:45:43 +0800

    [liuxing] implement assign

--- 3-object/assign/index.js
+++ 3-object/assign/index.js
@@ -1,3 +1,9 @@
 export default function addSerialNumber(source) {
-  // TODO 5: 在这里写实现代码，需要采用ES6 Object.assign
+  Object.assign(source.properties, {
+    status: 'processed'
+  });
+
+  return Object.assign(source, {
+    serialNumber: '12345'
+  });
 }
