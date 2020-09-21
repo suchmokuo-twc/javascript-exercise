21d9dbd0d28faf5a504acc55b5bd53980a95afbd
SUCHMOKUO <suchmokuo@gmail.com>
2020-09-21 16:54:53 +0800

    [liuxing] implement class_practice_2

--- 4-class/practice_2/person.js
+++ 4-class/practice_2/person.js
@@ -1,1 +1,12 @@
-// TODO 10: 在这里写实现代码
+export default class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  introduce() {
+    const { name, age } = this;
+
+    return `My name is ${name}. I am ${age} years old.`;
+  }
+}
--- 4-class/practice_2/student.js
+++ 4-class/practice_2/student.js
@@ -1,1 +1,15 @@
-// TODO 11: 在这里写实现代码
+import Person from './person';
+
+export default class Student extends Person {
+  constructor(name, age, klass) {
+    super(name, age);
+
+    this.klass = klass;
+  }
+
+  introduce() {
+    const { klass } = this;
+
+    return `I am a Student. I am at Class ${klass}.`;
+  }
+}
